8741c94b59ae4577c457a5b64eaa3c8480093366
leejkee <leejykee@yeah.net>
2025-10-06 11:39:31 +0800

    Projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            THINGS I HAVE BUILT
+            Things I Have Built.
           </p>
         </div>
         <div className="container py-12">
